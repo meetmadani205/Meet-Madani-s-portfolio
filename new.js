4c35e6ce8ea55da260dd89565c0cf5e978e988b4
meetmadani205 <142378084+meetmadani205@users.noreply.github.com>
2024-02-16 21:40:35 +0530

    Update new.js

--- new.js
+++ new.js
@@ -1,54 +1,18 @@
+alert("Hello there! Welcome to Meet Madani's portfolio!");
 
-alert("Welcome to Meet Madani's portfolio!");
 
-let btn = document.querySelector("#js");
-let mode = "light";
-let body = document.querySelector("body");
-let nav = document.querySelector("nav");
-let foot = document.querySelector("footer");
-let fofoot = document.querySelector("#foot");
-let an = nav.getElementsByTagName("a");
-let line = document.getElementsByTagName("hr");
-
-btn.addEventListener('click',() =>
-{
-    if(mode=="light")
-    {
-        body.style.backgroundColor = "black";
-        btn.innerText = "Turn to light mode";
-        mode = "dark";
-        body.style.color = "white";
-        nav.style.backgroundColor = "white";
-        foot.style.backgroundColor = "white";
-        fofoot.style.color = "black";
-        for(var i=1;i<an.length;i++)
-        {
-          an[i].style.color = "black";
-        }
-        line.style.border = "1 px solid white";
-    }
-    else
-    {
-      body.style.backgroundColor = "white";
-      btn.innerText = "Turn to dark mode";
-      mode="light";
-      body.style.color = "black";
-      nav.style.backgroundColor = "black";
-      foot.style.backgroundColor = "black";
-      fofoot.style.color = "white";
-      for(var i=1;i<an.length;i++)
-        {
-          an[i].style.color = "white";
-        }
-        line.style.border = "1 px solid black";
-    }
-    }
-
-)
-
-{
-  var typed = new Typed('#typed', {
-    strings: ['Web devlopment.', '&amp;DSA.','&amp;Problem solving.'],
+var typed = new Typed('#typed', {
+    strings: ['Web devlopment.','&amp;Java programming' ,'&amp;DSA.','&amp;Problem solving.'],
     typeSpeed: 50,
   });
-}
+
+
+let toggle = document.querySelector(".toggle");
+let nav = document.querySelector("nav");
+let ul = document.querySelector("ul");
+
+toggle.addEventListener('click', () => {
+  ul.classList.toggle('vcl-r');
+  nav.classList.toggle('hofnav-r');
+  
+})
