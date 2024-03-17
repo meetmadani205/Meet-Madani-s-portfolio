e395211fdeb88b48e521e75aeaf88e1e83d689f5
Meet Madani <142378084+meetmadani205@users.noreply.github.com>
2024-03-17 12:03:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+
+var typed = new Typed('#typed', {
+    strings: ['Frontend Web devlopment.','Java programming.' ,'DSA.','Problem solving.'],
+    typeSpeed: 90,
+    loop : true,
+  });
+
+
+let toggle = document.querySelector(".toggle");
+let nav = document.querySelector("nav");
+let ul = document.querySelector("ul");
+let menuIcon = document.querySelector("#menu-icon");
+
+toggle.addEventListener('click', () => {
+  ul.classList.toggle('vcl-r');
+  nav.classList.toggle('hofnav-r');
+  
+})
+
+menuIcon.addEventListener('click',  () => {
+    menuIcon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active');
+    header.classList.toggle('scroll', navbar.classList.contains('active'));
+})
